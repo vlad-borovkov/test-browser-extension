4d876bf462daeee210ed03000936b7ec350d052a
vlad-borovkov <borovkov-vlad@ya.ru>
2026-02-18 23:28:39 +0500

    init

--- dist/src/background/background.js
+++
@@ -1,1 +0,0 @@
-chrome.runtime.onInstalled.addListener(()=>{console.log("Расширение успешно установлено!"),chrome.storage.local.set({color:"#3aa757"}).then(o=>{console.log(o,"Успешно установлено")})});
