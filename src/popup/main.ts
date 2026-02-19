384343ebff2ac63e8d4681954107031a480a9dd4
vlad-borovkov <borovkov-vlad@ya.ru>
2026-02-19 13:43:13 +0500

    fix: build-and-file-structure

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -4,12 +4,14 @@ btn.addEventListener('click', async () => {
     console.log('clicked');
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    if (tab.id) {
+    if (tab.id && tab.url && !tab.url.startsWith('chrome://')) {
         await chrome.scripting.executeScript({
             target: {tabId: tab.id},
             func: () => {
                 document.body.style.backgroundColor = 'orange';
             }
         });
+    } else if (tab.url?.startsWith('chrome://')) {
+        console.warn('Cannot inject scripts into chrome:// pages.');
     }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,17 @@ export default defineConfig({
         rollupOptions: {
             // Определяем несколько точек входа
             input: {
-                popup: resolve(__dirname, 'src/popup/index.html'),
+                popup: resolve(__dirname, 'popup.html'),
                 background: resolve(__dirname, 'src/background/main.ts'),
                 content: resolve(__dirname, 'src/content/main.ts'),
             },
             output: {
                 // Убираем хеши из имен файлов, чтобы manifest.json всегда ссылался на правильные пути
-                entryFileNames: 'src/[name]/[name].js',
+                entryFileNames: '[name].js',
                 chunkFileNames: 'assets/[name].js',
                 assetFileNames: 'assets/[name].[ext]',
             },
         },
     },
+    base: './',
 });
